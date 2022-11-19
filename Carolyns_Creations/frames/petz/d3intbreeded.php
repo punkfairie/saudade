f5081ca984523e1a527cf03b8c9125442263fba7
Marley <marleyrae1997@gmail.com>
2022-11-18 22:03:29 -0800

    petz/d3intbreeded

---
+++ Carolyns_Creations/frames/petz/d3intbreeded.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Special sound-stripped originals</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="normal brown center small-top-gap">
+    <p class="bigger purple">
+        NOTE: No hexing ability is needed at all!<br>
+        These aren't just for people who want to edit petz. They are also for anyone who has
+        downloaded a breed in the form of an external LNZ file and wants to use it in their game.
+    </p>
+
+    <p class="bigger purple">
+        For details of where the files go in your game,
+        <a href="../tutorials/LNZBreedzHowto.php">click here</a>
+    </p>
+
+    <p>
+        <span class="bigger purple">
+            <b>Sound-stripped, external-lnz Original Breedz for Dogz 3 (non-English)</b><br>
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        </span><br>
+        These breedz are the original P.F.Magic or Studio Mythos originals. The Pug is the version
+        that I "fixed" so that the tongue is properly attached. Apart from that, all that I've done
+        in that and the other breedz is to make it so that the sounds and .lnz files that were
+        internal are now external. The external .lnz files will allow people who are afraid of hex
+        editors to use Notepad only for making litterz and overwriting breedz. The sounds are
+        external so that, if the sounds have been downloaded once, all breed downloads can be
+        sound-stripped and very small.<br>
+        If you only want the external-sound versions of these breedz, scroll further down this page.
+        <br>
+        Please note that the readme in with the breedfile explains where everything should go,
+        including the sounds that are downloadable from
+        <a href="../sounds/soundsorig.php" title="Go here to get sounds">here.</a><br>
+
+        <a href="dl/d3intbreeded_ExtLnzSSBulldogInt_d3.zip">Bulldog</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSChihuahuaInt_d3.zip">Chihuahua</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSDachshundInt_d3.zip">Dachshund</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSDalmatianInt_d3.zip">Dalmatian</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSGermanshepherdInt_d3.zip">German Shepherd</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSGoldenretrieverInt_d3.zip">Golden Retriever</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSGreatdaneInt_d3.zip">Great Dane</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSJackrussellInt_d3.zip">Jack Russell</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSLabradorInt_d3.zip">Labrador</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSMuttInt_d3.zip">Mutt</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSPapillonInt_d3.zip">Papillon</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSPoodleInt_d3.zip">Poodle</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSPugInt_d3.zip">Pug</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSScottieInt_d3.zip">Scottie</a><br>
+        <a href="dl/d3intbreeded_ExtLnzSSSheepdogInt_d3.zip">Sheepdog</a>
+    </p>
+
+    <p>
+        <span class="bigger purple">
+            <b>Sound-stripped Original Breedz for Dogz 3</b><br>
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        </span><br>
+        These files are sound-stripped only, for people who do not want the externally-editable .lnz
+        files but do want the sounds separate.<br>
+        Please note that the readme in with the breedfile explains where to put the sounds that are
+        downloadable also, from
+        <a href="../sounds/soundsorig.php" title="Go here to get sounds">here.</a><br>
+
+        <a href="dl/d3intbreeded_SSBulldogInt_d3.zip">Bulldog</a><br>
+        <a href="dl/d3intbreeded_SSChihuahuaInt_d3.zip">Chihuahua</a><br>
+        <a href="dl/d3intbreeded_SSDachshundInt_d3.zip">Dachshund</a><br>
+        <a href="dl/d3intbreeded_SSDalmatianInt_d3.zip">Dalmatian</a><br>
+        <a href="dl/d3intbreeded_SSGermanshepherdInt_d3.zip">German Shepherd</a><br>
+        <a href="dl/d3intbreeded_SSGoldenretrieverInt_d3.zip">Golden Retriever</a><br>
+        <a href="dl/d3intbreeded_SSGreatdaneInt_d3.zip">Great Dane</a><br>
+        <a href="dl/d3intbreeded_SSJackrussellInt_d3.zip">Jack Russell</a><br>
+        <a href="dl/d3intbreeded_SSLabradorInt_d3.zip">Labrador</a><br>
+        <a href="dl/d3intbreeded_SSMuttInt_d3.zip">Mutt</a><br>
+        <a href="dl/d3intbreeded_SSPapillonInt_d3.zip">Papillon</a><br>
+        <a href="dl/d3intbreeded_SSPoodleInt_d3.zip">Poodle</a><br>
+        <a href="dl/d3intbreeded_SSPugInt_d3.zip">Pug</a><br>
+        <a href="dl/d3intbreeded_SSScottieInt_d3.zip">Scottie</a><br>
+        <a href="dl/d3intbreeded_SSSheepdogInt_d3.zip">Sheepdog</a>
+    </p>
+</div>
+
+</body>
+</html>

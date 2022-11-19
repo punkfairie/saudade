8fb8e983827ce56405f2a6051d4bc3d851a785b3
Marley <marleyrae1997@gmail.com>
2022-11-18 22:15:46 -0800

    petz/p2breedz.php

---
+++ Carolyns_Creations/frames/petz/p2breedz.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>New Petz 2 Breedz</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="big green center more-side-padding">
+    <p>
+        <span class="bigger">
+            <b>New Petz 2 Breedz</b><br>
+            Click breed photo or name to download.
+        </span><br>
+        <a href="../sounds/sounds.php" title="Go here to get sounds">Go here for Species sounds</a>
+        <br>
+        Various Species or Fantasy breedz need sounds; if you want them to sound right in your game,
+        you will have to download the sounds as well. (note -- the bunny's sounds are included in
+        the bunny breed zip)
+    </p>
+
+    <p>
+        My Skorch Odd-petz breed is now <a href="dl/p2breedz_skorchc2.zip">available here</a>. All
+        13 of the originals have now been brought into the Catz games.
+    </p>
+</div>
+
+<table class="center center-table big">
+    <tr>
+        <td>
+            <a href="dl/p2breedz_Walret_c2.zip" title="Download Walret for Catz 2">
+                <img src="images/p2breedz_Walret.gif" alt="Walret"><br>
+                Walret (Catz)
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/p2breedz_102_c2.zip" title="Download 102 for Catz 2">
+                <img src="images/p2breedz_102dalmatians.gif" alt="102"><br>
+                102 (Catz)
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/p2breedz_Jester_c2.zip" title="Download Jester for Catz 2">
+                <img src="images/p2breedz_Jester.gif" alt="Jester"><br>
+                Jester (Catz)
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/p2breedz_Lips_c2.zip" title="Download Lips for Catz 2">
+                <img src="images/p2breedz_Lips.gif" alt="Lips"><br>
+                Lips (Catz)
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <a href="dl/p2breedz_Bunny_c2.zip" title="Download Bunny for Catz 2">
+            <img src="images/p2breedz_Bunnyp2.gif" alt="Bunny"><br>
+            Bunny (Catz)
+        </a>
+    </tr>
+</table>
+
+</body>
+</html>

e8c50ce340adb4efe317a0c7a43b7a924744ff5c
Marley <marleyrae1997@gmail.com>
2022-11-19 11:54:18 -0800

    petz/c2breedz.php

---
+++ Carolyns_Creations/frames/petz/c2breedz.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Catz 2 Breedz</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="big green center more-side-padding">
+    <p class="bigger">
+        Catz 2 Breedz<br>
+        Click breed photo or name to download.
+    </p>
+
+    <p>
+        <a href="../sounds/sounds.php" title="Go here to get sounds">Go here for Species sounds</a>
+        <br>
+        Various Species or Fantasy breedz need sounds; if you want them to sound right in your game,
+        you will have to download the sounds as well.
+    </p>
+</div>
+
+<table class="center-table center green big">
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Appaloosa_c2.zip" title="Download Appaloosa for Catz 2">
+                <img src="images/c2breedz_Appaloosa.gif" alt="Appaloosa"><br>
+                Appaloosa
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Chickenz_c2.zip" title="Download Chickenz for Catz 2">
+                <img src="images/c2breedz_Chickenz.gif" alt="Chickenz"><br>
+                Chickenz With "Eggz"
+            </a>/
+            <a href="dl/c2breedz_ChickenzNoEggz_c2.zip"
+               title="Download Chickenz No Eggz">Without "Eggz"</a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Dragonair_c2.zip" title="Download Dragonair for Catz 2">
+                <img src="images/c2breedz_Dragonair.gif" alt="Dragonair"><br>
+                Dragonair
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Dynaroo_c2.zip" title="Download Dynaroo for Catz 2">
+                <img src="images/c2breedz_Dynaroo.gif" alt="Dynaroo"><br>
+                Dynaroo
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Grinnz_c2.zip" title="Download Grinnz for Catz 2">
+                <img src="images/c2breedz_Grinnz.gif" alt="Grinnz"><br>
+                Grinnz
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Himalayan_c2.zip" title="Download Himalayan for Catz 2">
+                <img src="images/c2breedz_Himalayan.gif" alt="Himalayan"><br>
+                Himalayan
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Honkbirdie_c2.zip" title="Download Honkbirdie for Catz 2">
+                <img src="images/c2breedz_Honkbirdie.gif" alt="Honkbirdie"><br>
+                Honkbirdie
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Honker_c2.zip" title="Download Honker for Catz 2">
+                <img src="images/c2breedz_Honker.gif" alt="Honker"><br>
+                Honker
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Modvark_c2.zip" title="Download Modvark for Catz 2">
+                <img src="images/c2breedz_Modvark.gif" alt="Modvark"><br>
+                Modvark
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Norvil_c2.zip" title="Download Norvil for Catz 2">
+                <img src="images/c2breedz_Norvil.gif" alt="Norvil"><br>
+                Norvil
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Quadrapus_c2.zip" title="Download Quadrapus for Catz 2">
+                <img src="images/c2breedz_Quadrapus.gif" alt="Quadrapus"><br>
+                Quadrapus
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Reindeerz_c2.zip" title="Download Reindeerz for Catz 2">
+                <img src="images/c2breedz_Reindeerz.gif" alt="Reindeerz"><br>
+                Reindeerz
+            </a>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <a href="dl/c2breedz_Snowbo_c2.zip" title="Download Snowbo for Catz 2">
+                <img src="images/c2breedz_Snowbo.gif" alt="Snowbo"><br>
+                Snowbo
+            </a>
+        </td>
+
+        <td>
+            <a href="dl/c2breedz_Zott_c2.zip" title="Download Zott for Catz 2">
+                <img src="images/c2breedz_Zott.gif" alt="Zott"><br>
+                Zott
+            </a>
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>

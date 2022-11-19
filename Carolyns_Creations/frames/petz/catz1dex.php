0408cd1c0914c153b378620f070dc6fbf971f94f
Marley <marleyrae1997@gmail.com>
2022-11-19 14:20:01 -0800

    petz/catz1dex.php

---
+++ Carolyns_Creations/frames/petz/catz1dex.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Catz Your Computer Petz</title>
+    <!-- -->
+</head>
+<body id="catz1dex">
+<?php include '../../../_menu.php' ?>
+
+<div class="more-side-padding center big bold">
+    <img src="images/catz1dex_catzlogo.gif" alt="Welcome to Catz Home Page!">
+    <img src="images/catz1dex_kitten.gif"><br>
+    Catz are the next in line of Your Computer Petz,<br>
+    following in the successful pawprints of Dogz.<br>
+    Catz live on your computer and they don't mind sharing it with you.<br>
+    You can play with them and watch them stalk the mouse across your documents.<br>
+    In screen-saver mode they enjoy taking CatNapz.
+
+    <p>
+        <a href="sclub.php">Get to know </a>the Catz while
+        <a href="dl/catz1dex_catzinst.zip" target="_BLANK">downloading </a>the FREE adoption kit.
+        <br>
+        When you've got the demo, click the "Download Catz" button below to download the "fooler".
+        <br>
+    </p>
+
+    <p>
+        <a href="dload2.php"><img src="images/catz1dex_1_downld.gif"></a>
+
+        <a href="sclub.php"><img src="images/catz1dex_1_club.gif"></a>
+    </p>
+
+    <p class="left-align normal" style="width: 70%; margin: 0 auto; overflow: auto;">
+        <img src="images/catz1dex_dog.gif" class="float-left" style="margin-right: 40px;">
+        Be sure to check out the world's first<br>
+        Computer Petz too --<br>
+        get the demo from
+        <a href="https://web.archive.org/web/20020608010715/http://www.dmwright.com/oddpetz/"
+           target="_BLANK">Daniel's</a> or
+        <a href="https://web.archive.org/web/20091002091450/http://www.minibytepetz.net/Minibyte_Petz/home.html"
+           target="_BLANK">Minibyte's</a><br>
+        and remember you can download a<a href="dogz1fooler.php"> demo fooler</a> for it also.
+    </p>
+
+    <p style="height: 120px;">
+        <img style="vertical-align: middle;" src="images/catz1dex_ns3now.gif">
+        <img style="vertical-align: middle;" src="images/catz1dex_pfmlogo.gif">
+        <img style="vertical-align: middle;" src="images/catz1dex_msnow.gif">
+    </p>
+
+    <p class="bold normal">
+        © 1996
+        <a href="https://web.archive.org/web/20031123085457/http://www.quvu.net/interactivestory.net/"
+           target="_BLANK">PF Magic</a><br>
+        This Site Best Viewed with<br>
+        Netscape 3.0 [ATLAS]<br>
+        and Microsoft Explorer 2.0
+    </p>
+
+    <img src="images/catz1dex_wdg3.gif">
+</div>
+
+</body>
+</html>

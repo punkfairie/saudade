8521d4b2f266fdacdd136d1cdc60f99c0fb13f32
Marley <marleyrae1997@gmail.com>
2022-11-19 17:32:26 -0800

    babyz/babyznew.php

---
+++ Carolyns_Creations/frames/babyz/babyznew.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>New Babyz</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="big center green">
+    <p class="bigger">
+        <b>New Babyz</b><br>
+        Click photo or name to download.
+    </p>
+
+    <p class="bigger">
+        <b>NEW!!</b>For the Pasted-Head babyz, go to the
+        <a href="babyzheadbanger.php">Headbanger babyz</a> page<br>
+        For the Faerie babyz, go to the <a href="faeriebabyz.php">Faerie babyz</a> page<br>
+        For the Oddballz babyz, go to the <a href="babyzoddball.php">Babyz Oddballz</a> page<br>
+        For the Fishies, go to the <a href="babyzfish.php">Babyz Fishies</a> page<br>
+        For the butterflies and spiders, go to the
+        <a href="babyzarthropods.php">Babyz Arthropods</a> page<br>
+        For the Valentine Hearts, go to the <a href="babyzhearts.php">Babyz Hearts</a> page<br>
+        For the anatomically correct babyz, go to the <a href="realbabyz.php">Real Babyz</a> page
+        <br>
+        For the Pharaoh babyz, go to the <a href="pharaohbabyz.php">Pharaoh babyz</a> page<br>
+        For the Growing babyz, go to the <a href="growerbabyz.php">Grower babyz</a> page<br>
+        For babyz with flowers/wings in their hair --
+        <a href="wingflowerbabyz.php">Flower-power babyz</a> page<br>
+        More babyz with jewels, hair-wings or necklets --
+        <a href="wingjewelbabyz.php">Jewel babyz</a> page<br>
+        <span class="normal">
+            Afrasweet and Miggles are on the jewel babyz page also. Hey, it's my site, I can do
+            things like that :-)
+        </span><br>
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    </p>
+
+    <p>Here are a few unusual boy babyz for you. Each is distinctive in a special way...</p>
+
+    <p>
+        <a href="dl/babyznew_Xmyboy.zip" title="Xmyboy">
+            <img src="images/babyznew_Xmyboy.gif" alt="Xmyboy"><br>
+            Xmyboy
+        </a><br>
+        Xmyboy has a very unusual hairstyle that shimmers as he moves
+    </p>
+
+    <p>
+        <a href="dl/babyznew_Shock2Lock.zip" title="Shock morphing to Lock">
+            <img src="images/babyznew_ShockMorphtoLock.gif" alt="ShockMorphtoLock"><br>
+            Shock2Lock
+        </a><br>
+        Shock2Lock starts as Shock and, as the days of play pass in the game he morphs into Lock.
+        Thanks to Nika for the idea of a Shock and Lock baby :-)
+    </p>
+
+    <p>
+        <a href="dl/babyznew_Tigger.zip" title="Tigger">
+            <img src="images/babyznew_Tigger.gif" alt="Tigger"><br>
+            Tigger
+        </a><br>
+        Tigger has a tail which moves around as he moves. If you want to see him in his natural
+        state, as shown in the picture, you will need to remove the White Diaper.clo file from the
+        game's clothes directory.
+    </p>
+
+    <p>.</p>
+</div>
+
+</body>
+</html>

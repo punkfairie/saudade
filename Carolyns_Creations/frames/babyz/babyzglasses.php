e5d2cc4ea8e837e6cdc893547c3839ec5cce36f6
Marley <marleyrae1997@gmail.com>
2022-11-19 21:34:05 -0800

    babyz/babyzglasses.php

---
+++ Carolyns_Creations/frames/babyz/babyzglasses.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Babyz Glasses</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="big center brown">
+    <p class="bigger purple">.</p>
+
+    <p class="bigger purple">
+        <b>unusual glasses for your Babyz</b><br>
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    </p>
+
+    <p>
+        These glasses were inspired by a request by Budgie, on the
+        <a href="https://web.archive.org/web/20070208055821/http://s4.invisionfree.com/Insatiable/"
+           target="_TOP">Insatiable forum</a>, who wanted star-shaped glasses. Later on in the
+        thread, Chris suggested heart-shaped sunglasses for babyz on the beach... It caught my
+        imagination and here, at last they are for download along with a very special babyz hexed by
+        Tigger.
+    </p>
+
+    <p>
+        In this montage by Tigger,<br>
+        <a href="dl/babyzglasses_EltonJohnBaby.zip" title="Floretta">
+            <img src="images/babyzglasses_EltonJohnCollage.jpg" alt="Elton John"><br>
+        Tigger's Elton John</a> is modelling<br>
+        <a href="babyzglasses_Heartglasses.zip" title="Heart Glasses">my Heart glasses and</a><br>
+        <a href="dl/babyzglasses_Eltonglasses.zip" title="Elton Glasses">my Elton Glasses</a><br>
+        Click on the picture to download him. He looks best if you also have installed my Elton
+        Glasses and<br>
+        clothes from
+        <a href="http://www.geocities.com/bubble_babyz_boutique/" class="defunct"
+           target="_TOP">Liz' Bubble Babyz boutique</a>. Read the readme in his zipfile :-)
+    </p>
+
+    <p>
+        Also for your downloading pleasure,
+        <a href="dl/babyzglasses_Blankglasses.zip"
+           title="Blank Glasses">here is another pair of glasses</a> which is really three (or even
+        more) in one! Please read the readme very carefully and install it properly, or it won't
+        work for you. Hexers may also enjoy this one. Here are three of my own babes, hexed for me
+        by some lovely people, modelling the set for you:<br>
+        <img src="images/babyzglasses_WackyStarGlasses.gif" alt="wacky stars">
+        <img src="images/babyzglasses_PlainHeartGlasses.gif" alt="plain hearts">
+        <img src="images/babyzglasses_Plaincircleglasses.gif" alt="palin circles"><br>
+        Delissa, from DStar, favours the Wacky Star glasses; Dream, from Garfield, has a classier
+        taste in hearts; and Jane's Serenity seems rather fond of the simple circlets.
+    </p>
+
+    <p>Enjoy!</p>
+
+    <p>.</p>
+</div>
+
+</body>
+</html>

c5fe44bb983dc6aecc5591f367fcf26c7832ef7e
Marley <marleyrae1997@gmail.com>
2022-11-19 20:33:26 -0800

    babyz/babyz-1.php

---
+++ Carolyns_Creations/frames/babyz/babyz-1.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Hex edited babyz</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="small-gap-top bigger purple center">
+    <p>
+        <b>Hex edited babyz</b><br>
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    </p>
+</div>
+
+<div class="border-outer">
+    <div class="border-inner center brown bigger bold">
+        <p>
+            These are my Babyz. You need to extract the .baby file into your Adopted Babyz directory
+            in your Babyz program directory, or if you have 3 Babyz in that directory already, into
+            you Grandma's House directory. Click on the picture to download.
+        </p>
+
+        <p>
+            Also, all Babyz here have diapers matching their skin tones, and the pictures have been
+            taken with those diapers in place. If you want to download the diapers also, go to my
+            <a href="babyzclothes.php">babyz clothes</a> page.
+        </p>
+
+        <p>
+            <u>Page</u>:
+            1
+            <a href="babyz-2.php">2</a>
+            <a href="babyz-3.php">3</a>
+            <a href="babyz-4.php">4</a>
+            <a href="babyz-5.php">5</a>
+            |
+            <a href="babyzspecial-1.php">Special Babyz</a>
+            |
+            <a href="babyzplay.php">Play with a Babyz Online</a>
+        </p>
+
+        <table class="center wide-center-table img-border">
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Alain.zip">
+                        <img src="images/babyz-1_Alain.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Alaina.zip">
+                        <img src="images/babyz-1_Alaina.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Allan.zip">
+                        <img src="images/babyz-1_Allan.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Andrea.zip">
+                        <img src="images/babyz-1_Andrea.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Angiefay.zip">
+                        <img src="images/babyz-1_Angiefay.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Anona.zip">
+                        <img src="images/babyz-1_Anona.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Anonny.zip">
+                        <img src="images/babyz-1_Anonny.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Ariana.zip">
+                        <img src="images/babyz-1_Ariana.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Baby1.zip">
+                        <img src="images/babyz-1_Baby1.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Baby2.zip">
+                        <img src="images/babyz-1_Baby2.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Baby3.zip">
+                        <img src="images/babyz-1_Baby3.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Baby3x.zip">
+                        <img src="images/babyz-1_Baby3x.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Baby4.zip">
+                        <img src="images/babyz-1_Baby4.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Bessalia.zip">
+                        <img src="images/babyz-1_Bessalia.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Blondie.zip">
+                        <img src="images/babyz-1_Blondie.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Blondie2.zip">
+                        <img src="images/babyz-1_Blondie2.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Blossom.zip">
+                        <img src="images/babyz-1_Blossom.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Bluebabe.zip">
+                        <img src="images/babyz-1_Bluebabe.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Bluebabe0.zip">
+                        <img src="images/babyz-1_Bluebabe0.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Bluefay.zip">
+                        <img src="images/babyz-1_Bluefay.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Boybase.zip">
+                        <img src="images/babyz-1_Boybase.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Caitlina.zip">
+                        <img src="images/babyz-1_Caitlina.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Caitlinb.zip">
+                        <img src="images/babyz-1_Caitlinb.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Candyfloss.zip">
+                        <img src="images/babyz-1_Candyfloss.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Carmena.zip">
+                        <img src="images/babyz-1_Carmena.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Carolyna.zip">
+                        <img src="images/babyz-1_Carolyna.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Cester.zip">
+                        <img src="images/babyz-1_Cester.gif">
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="dl/babyz-1_Charlemagne.zip">
+                        <img src="images/babyz-1_Charlemagne.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Charmian.zip">
+                        <img src="images/babyz-1_Charmian.gif">
+                    </a>
+                </td>
+
+                <td>
+                    <a href="dl/babyz-1_Cherrala.zip">
+                        <img src="images/babyz-1_Cherrala.gif">
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<div class="bigger purple center">
+    <u>Page</u>:
+    1
+    <a href="babyz-2.php">2</a>
+    <a href="babyz-3.php">3</a>
+    <a href="babyz-4.php">4</a>
+    <a href="babyz-5.php">5</a>
+    |
+    <a href="babyzspecial-1.php">Special Babyz</a>
+    |
+    <a href="babyzplay.php">Play with a Babyz Online</a>
+</div>
+
+</body>
+</html>

8ea8078f391b525ee37f4bc3d0f056b7d38d3041
Marley <marleyrae1997@gmail.com>
2022-11-19 21:43:55 -0800

    babyz/babyzharem.php

---
+++ Carolyns_Creations/frames/babyz/babyzharem.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- from source -->
+    <title>Babyz Harem sets</title>
+    <!-- -->
+</head>
+<body>
+<?php include '../../../_menu.php' ?>
+
+<div class="center brown big">
+    <p class="bigger purple">.</p>
+
+    <p class="bigger purple">
+        <b>My Babyz Harem sets</b><br>
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    </p>
+
+    <p>
+        These "harem" items are just a bit of fun, in case anyone is upset by the idea. My babyz
+        love dressing up, and they seem to enjoy my more glittery or diaphanous items. I usually
+        have three babyz in the game at once, and often it is one boy and two girls or vice versa. I
+        think the funniest time was when I had two girls in the game who were having fun with a pile
+        of clothes -- and they dressed Rameses in harem pants and a belly-bow... LOL...<br>
+        Here you see Anniebear's<br>
+        <img src="images/babyzharem_TevynGoldDanceDress.gif" alt="Tevyn"><br>
+        adorable Tevyn modelling the gold dress along with the spiky head-dress and a belly jewel.
+        You can see that she enjoys belly-dancing!<br>
+        <a href="dl/babyzharem_GoldHaremDress.zip" title="Gold Harem dress">Gold Harem dress</a><br>
+        <img src="images/babyzharem_haremdress1.gif" alt="gold harem dress">
+    </p>
+
+    <p>
+        Here you see my lovely Callys, from Lain<br>
+        <img src="images/babyzharem_Callys.gif" alt="Callys"><br>
+        modelling the spiky collar and head-dress along with a belly bow. The white diaper is not a
+        harem item :-)<br>
+        <a href="dl/babyzharem_spikyheaddress.zip" title="spikyheaddress">spiky headdress</a><br>
+        <a href="dl/babyzharem_spikyneckcollar.zip" title="spikyneckcollar">spiky collar</a><br>
+        <a href="dl/babyzharem_goldnavelbow.zip"
+           title="goldnavelbow.zip">gold and ruby navel-bow</a><br>
+        <a href="dl/babyzharem_goldnavelgem.zip" title="goldnavelgem">gold navel gem</a>
+    </p>
+
+    <p>More to come...</p>
+
+    <p>Enjoy!</p>
+
+    <p>.</p>
+</div>
+
+</body>
+</html>

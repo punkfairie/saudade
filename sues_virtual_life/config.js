6621ccd0cbf50f953b6cbbf85a9efa76e01f038b
marleyrae <marleyrae@protonmail.com>
2023-06-14 12:18:54 -0700

    sues_virtual_life: index

---
+++ sues_virtual_life/config.js
@@ -0,0 +1,46 @@
+let currentPage = location.pathname.split('/').at(-2)
+
+const waybackSiteBase = 'http://boardwalkerz.com/svl/'
+
+const pageMetadata = {
+    sues_virtual_life: {
+        captureId: '20150810211140',
+        sourcePageName: '',
+    },
+    past: {
+        captureId: '',
+        sourcePageName: 'past.html',
+    },
+    links: {
+        captureId: '',
+        sourcePageName: 'links.html',
+    },
+    catz: {
+        captureId: '',
+        sourcePageName: 'catz.html',
+    },
+    dogz: {
+        captureId: '',
+        sourcePageName: 'dogz.html',
+    },
+    other: {
+        captureId: '',
+        sourcePageName: 'other.html',
+    },
+    babyz: {
+        captureId: '',
+        sourcePageName: 'babyz.html',
+    },
+    archive: {
+        captureId: '',
+        sourcePageName: 'archive.html',
+    },
+}
+
+export const wayback =
+    pageMetadata[currentPage].captureId
+    + '/'
+    + waybackSiteBase
+    + pageMetadata[currentPage].sourcePageName
+
+
--- sues_virtual_life/gulpfile.js
+++ sues_virtual_life/gulpfile.js
@@ -9,7 +9,13 @@ function html() {
     return src(htmlFiles)
         .pipe(posthtml([
             require('posthtml-urls')({
-                eachURL: (url) => url[0] === '/' && url !== '/menu.js' ? `/sues_virtual_life${url}` : url,
+                eachURL: (url) => {
+                    if (url[0] === '/' && url !== '/menu.js') {
+                        return `/sues_virtual_life${url}`
+                    } else {
+                        return url
+                    }
+                },
             }),
         ]))
         .pipe(rename(path => {
@@ -23,4 +29,5 @@ exports.default = function () {
     browserSync.init(require('../bs-config'))
 
     watch(htmlFiles, {ignoreInitial: false}, html)
+    watch('sues_virtual_life.css').on('change', browserSync.reload)
 }
